4a75d251dc4f8e97f34a6cab85e4395f9e2ba125
Jayson Concha <jmconcha.pro@gmail.com>
2022-09-20 16:47:00 +0800

    cart add case

--- src/Data/DataSource/redux/reducers/cart.ts
+++ src/Data/DataSource/redux/reducers/cart.ts
@@ -12,7 +12,15 @@ interface Product {
 function cartReducer(state: Product[] = [], action: AnyAction) {
   switch (action.type) {
     case CART.ADD:
-      return [...state, ...action.payload];
+      return [
+        ...state,
+        {
+          id: action.payload.id,
+          name: action.payload.name,
+          price: action.payload.price,
+          quantity: action.payload.quantity,
+        },
+      ];
     case CART.REMOVE:
       return state.filter((product) => product.id === action.payload.id);
     default:
