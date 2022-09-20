59bcac7168335a139d4bcd1a5fc2b11679ccdec3
Jayson Concha <jmconcha.pro@gmail.com>
2022-09-20 14:49:26 +0800

    setup base routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
+import ProductList from './Presentation/Views/Product/List/ProductList';
+import CartProductList from './Presentation/Views/Cart/ProductList/CartProductList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/cart" element={<CartProductList />} />
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/Presentation/Views/Cart/ProductList/CartProductList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CartProductList() {
+  return <div>CartProductList</div>;
+}
+
+export default CartProductList;
