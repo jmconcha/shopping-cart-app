6825aa8d3b6f985358bc274a2b60c6e5ba0e71e0
Jayson Concha <jmconcha.pro@gmail.com>
2022-09-20 16:47:29 +0800

    product reducers

--- src/Data/DataSource/redux/reducers/products.ts
+++ src/Data/DataSource/redux/reducers/products.ts
@@ -1,7 +1,31 @@
 import { AnyAction } from 'redux';
 
-function productsReducer(state = [], action: AnyAction) {
-  return state;
+import { PRODUCT } from '../types';
+
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+function productsReducer(state: Product[] = [], action: AnyAction) {
+  switch (action.type) {
+    case PRODUCT.ADD:
+      return [
+        ...state,
+        {
+          id: action.payload.id,
+          name: action.payload.name,
+          price: action.payload.price,
+          quantity: action.payload.quantity,
+        },
+      ];
+    case PRODUCT.REMOVE:
+      return state.filter((product) => product.id === action.payload.id);
+    default:
+      return state;
+  }
 }
 
 export default productsReducer;
